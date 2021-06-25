a71bdbbf99da1990b2c4a31f980ab25fb3e0f1a4
Kushagra2403 <singhkushagra2403@gmail.com>
2021-06-25 23:47:39 +0530

    Search Functionality Completed

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   return (
     <div className="d-flex justify-content-center my-4 search">
       <h4>Search for a Movie: </h4>
-      <input type="text" className="mx-3" placeholder="Enter text here" />
+      <input
+        type="text"
+        className="mx-3"
+        placeholder="Enter text here"
+        value={props.searchValue}
+        onChange={(e) => props.setSearchValue(e.target.value)}
+      />
     </div>
   );
 };
